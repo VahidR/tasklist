4ccb757b21436e12a33ef5990ba0724fa0bc34aa
Vahid Rafiei <vahid.rafiei@gmail.com>
2014-05-22 12:13:16 +0200

    some logic for adding a new task

---
+++ tasklist.js
@@ -0,0 +1,55 @@
+/*
+* not mess up with global window scope !
+*/
+"use strict";
+
+
+/*
+*  The main object for the entire application.
+*  OBS. It is NOT an object literal.. it is constructure based initialization
+*/
+function TaskAtHandApp () {
+  var version = "v1.0";
+
+  function setStatus (message) {
+    $("#app>footer").text(message);
+  }
+
+  this.start = function () {
+    $("#new-task-name").keypress(function(event){
+      if(event.which == 13) { // enter key
+        addTask();
+        return false;
+      }
+    }).focus();
+
+  function addTask() {
+    var taskName = $("#new-task-name").val();
+    if(taskName){
+      addTaskElement(taskName);
+      $("#new-task-name").val("").focus(); // reset the text field
+    }
+  }
+
+  function addTaskElement(taskName) {
+    var $task = $("<li></li>"); // the $ before variable is a good practice to remember it points to a jQuery object.
+    $task.text(taskName);
+    $("#task-list").append($task);
+  }
+
+
+
+
+};
+
+  $("#app header").append(version);
+  setStatus("ready")
+}
+
+/*
+*  Ignition point of application ... kind of main() in C lang
+*/
+$(function() {
+  window.app = new TaskAtHandApp();
+  window.app.start();
+});
